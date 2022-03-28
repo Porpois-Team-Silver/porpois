782a604a322f4ed1d29c2333fecf88c1c563094b
Devin Suttler <dsuttler28@gmail.com>
2022-03-28 15:42:08 -0700

    Basic activity created and data binding set up:
     - button and textView set up to test and to give an example of utilizing data binding.

---
+++ app/src/main/java/com/example/porpois/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.porpois;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.porpois.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set up data binding on this activity
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        TextView hiddenText = binding.hiddenText;
+        Button buttonReveal = binding.buttonReveal;
+
+        // toggle text to test data binding
+        buttonReveal.setOnClickListener(view -> {
+            if(hiddenText.getVisibility() != View.VISIBLE) {
+                hiddenText.setVisibility(View.VISIBLE);
+            }
+            else {
+                hiddenText.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
