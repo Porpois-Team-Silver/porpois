5b656b31051624cab9595fad5777edc5db101e89
Devin Suttler <dsuttler28@gmail.com>
2022-03-31 14:59:22 -0700

    Extremely rough skeleton. Only 1st four buttons work.
    This is just a rough outline of some possible logic. In no way
    is this final, nor does it have any design.

--- app/src/main/java/com/example/porpois/MainActivity.java
+++ app/src/main/java/com/example/porpois/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.porpois;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.porpois.databinding.ActivityMainBinding;
-
-import java.util.ArrayList;
+import com.example.porpois.taskPages.Connect;
+import com.example.porpois.taskPages.Give;
+import com.example.porpois.taskPages.Go;
+import com.example.porpois.taskPages.Make;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -22,17 +23,50 @@ public class MainActivity extends AppCompatActivity {
 
         // set up data binding on this activity
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        TextView hiddenText = binding.hiddenText;
-        Button buttonReveal = binding.buttonReveal;
 
-        // toggle text to test data binding
-        buttonReveal.setOnClickListener(view -> {
-            if(hiddenText.getVisibility() != View.VISIBLE) {
-                hiddenText.setVisibility(View.VISIBLE);
-            }
-            else {
-                hiddenText.setVisibility(View.INVISIBLE);
-            }
+        // connect button behaviors
+        Button connectButton = binding.connectButton;
+        connectButton.setOnClickListener(view -> {
+            Intent connectIntent = new Intent(MainActivity.this, Connect.class);
+            startActivity(connectIntent);
         });
+
+        // give button behaviors
+        Button giveButton = binding.buttonGive;
+        giveButton.setOnClickListener(view -> {
+            Intent giveIntent = new Intent(MainActivity.this, Give.class);
+            startActivity(giveIntent);
+        });
+
+        // go button behaviors
+        Button goButton = binding.goButton;
+        goButton.setOnClickListener(view -> {
+            Intent goIntent = new Intent(MainActivity.this, Go.class);
+            startActivity(goIntent);
+        });
+
+        // give button behaviors
+        Button makeButton = binding.makeButton;
+        makeButton.setOnClickListener(view -> {
+            Intent makeIntent = new Intent(MainActivity.this, Make.class);
+            startActivity(makeIntent);
+        });
+//
+//        // give button behaviors
+//        Button giveButton = binding.buttonGive;
+//        giveButton.setOnClickListener(view -> {
+//            Intent giveIntent = new Intent(MainActivity.this, Give.class);
+//            startActivity(giveIntent);
+//        });
+//
+//        // give button behaviors
+//        Button giveButton = binding.buttonGive;
+//        giveButton.setOnClickListener(view -> {
+//            Intent giveIntent = new Intent(MainActivity.this, Give.class);
+//            startActivity(giveIntent);
+//        });
+
+
+
     }
 }
